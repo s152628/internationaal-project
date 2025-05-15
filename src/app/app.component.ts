fee92211eb17222c27bec024dac159c2f237d83f
Miro Graré <miro.grare@student.ap.be>
2025-05-15 10:06:15 +0200

    add chatbot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LanguageSelectionComponent } from "./language-selection/language-selection.component";
+import { ChatbotComponent } from "./chatbot/chatbot.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, LanguageSelectionComponent],
+  imports: [RouterOutlet, NavbarComponent, LanguageSelectionComponent, ChatbotComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ZAS-WEBSITE';
---
+++ src/app/chatbot/chatbot.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-chatbot',
+  imports: [],
+  templateUrl: './chatbot.component.html',
+  styleUrls: ['./chatbot.component.css']
+})
+export class ChatbotComponent {
+
+}
